4bfba84579905c44bfe6617f01131485c5676427
Michael Freeborn <michaelfreeborn1@gmail.com>
2022-04-01 13:58:49 +0100

    add basic routing capability

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,13 +1,15 @@
 use aurora_alert_frontend::components::header::Header;
 use aurora_alert_frontend::components::main_content::MainContent;
+use aurora_alert_frontend::routes;
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
         <div class="container">
             <Header />
-            <MainContent />
+
         </div>
     }
 }
---
+++ frontend/src/routes.rs
@@ -0,0 +1,25 @@
+use yew::{function_component, html, prelude::*};
+use yew_router::prelude::*;
+
+#[derive(Clone, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: &Route) -> Html {
+    match routes {
+        Route::Home => html! { <h1>{ "Home" }</h1> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
+
+#[function_component(Router)]
+pub fn router() -> Html {
+    html! {<BrowserRouter>
+        <Switch<Route> render={Switch::render(switch)} />
+    </BrowserRouter>}
+}
