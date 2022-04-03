f45c1818cbb5e6540004090920519566073687dd
Michael Freeborn <michaelfreeborn1@gmail.com>
2022-04-03 23:11:31 +0100

    refactor registration form

--- frontend/src/types/user.rs
+++ frontend/src/types/user.rs
@@ -15,25 +15,9 @@ pub struct UnsubscribeParams {
 
 #[derive(Debug, Serialize, Clone)]
 pub struct UserRegisterInfo {
-    pub email: Option<String>,
+    pub email: String,
     pub alert_threshold: String,
     pub locations: std::collections::HashMap<String, i64>,
-}
-
-impl Default for UserRegisterInfo {
-    fn default() -> Self {
-        Self {
-            email: None,
-            alert_threshold: "yellow".to_string(),
-            locations: HashMap::new(),
-        }
-    }
-}
-
-impl UserRegisterInfo {
-    pub fn is_valid(&self) -> bool {
-        self.email.is_some() && !self.locations.is_empty()
-    }
 }
 
 #[derive(Deserialize, Debug, Clone, PartialEq)]
@@ -41,20 +25,16 @@ pub struct UserSubscribeWrapper {
     payload: String,
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Clone)]
 pub struct UserRegisterPostBody {
     pub email: String,
     pub alert_threshold: String,
     pub locations: Vec<i64>,
 }
 
-impl From<UserRegisterInfo> for UserRegisterPostBody {
-    fn from(other: UserRegisterInfo) -> Self {
-        Self {
-            email: other.email.unwrap(),
-            alert_threshold: other.alert_threshold.clone(),
-            locations: other.locations.values().cloned().collect(),
-        }
+impl UserRegisterPostBody {
+    pub fn is_valid(&self) -> bool {
+        !self.email.is_empty() && !self.locations.is_empty()
     }
 }
 
