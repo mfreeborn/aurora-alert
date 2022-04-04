aa6f7e96361dcd3ab7b6904c2955da64156a6f6c
Michael Freeborn <michaelfreeborn1@gmail.com>
2022-04-04 17:42:20 +0100

    suppress plotters for now

--- frontend/src/pages/home.rs
+++ frontend/src/pages/home.rs
@@ -1,39 +1,38 @@
-use plotters::prelude::*;
-use plotters_canvas::CanvasBackend;
+// // use plotters_canvas::CanvasBackend;
 use web_sys::HtmlCanvasElement;
 use yew::prelude::*;
 
 fn draw_chart(canvas: HtmlCanvasElement) {
-    let backend = CanvasBackend::with_canvas_object(canvas).expect("cannot find canvas element");
-    let root = backend.into_drawing_area();
+    // let backend = CanvasBackend::with_canvas_object(canvas).expect("cannot find canvas element");
+    // let root = backend.into_drawing_area();
 
-    root.fill(&WHITE).unwrap();
+    // root.fill(&WHITE).unwrap();
 
-    let power = 4_i32;
-    let mut chart = ChartBuilder::on(&root)
-        .margin(20_i32)
-        .x_label_area_size(30_i32)
-        .y_label_area_size(30_i32)
-        .build_cartesian_2d(-1f32..1f32, -1.2f32..1.2f32)
-        .unwrap();
+    // let power = 4_i32;
+    // let mut chart = ChartBuilder::on(&root)
+    //     .margin(20_i32)
+    //     .x_label_area_size(30_i32)
+    //     .y_label_area_size(30_i32)
+    //     .build_cartesian_2d(-1f32..1f32, -1.2f32..1.2f32)
+    //     .unwrap();
 
-    chart
-        .configure_mesh()
-        .x_labels(3)
-        .y_labels(3)
-        .draw()
-        .unwrap();
+    // chart
+    //     .configure_mesh()
+    //     .x_labels(3)
+    //     .y_labels(3)
+    //     .draw()
+    //     .unwrap();
 
-    chart
-        .draw_series(LineSeries::new(
-            (-50..=50)
-                .map(|x| x as f32 / 50.0)
-                .map(|x| (x, x.powf(power as f32))),
-            &RED,
-        ))
-        .unwrap();
+    // chart
+    //     .draw_series(LineSeries::new(
+    //         (-50..=50)
+    //             .map(|x| x as f32 / 50.0)
+    //             .map(|x| (x, x.powf(power as f32))),
+    //         &RED,
+    //     ))
+    //     .unwrap();
 
-    root.present().unwrap();
+    // root.present().unwrap();
 }
 
 #[function_component(Home)]
