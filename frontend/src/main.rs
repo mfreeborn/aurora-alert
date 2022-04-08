2fc3e94e8f5ffa4ec0450bcbb960988d4e93dc94
Michael Freeborn <michaelfreeborn1@gmail.com>
2022-04-08 16:23:01 +0100

    add performace/size optimisations for release mode

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -9,6 +9,10 @@ mod types;
 
 use app::App;
 use services::requests;
+use wee_alloc;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 fn main() {
     wasm_logger::init(wasm_logger::Config::default());
